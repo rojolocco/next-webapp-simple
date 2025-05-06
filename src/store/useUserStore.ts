eff8baa2e1033d4b9bd9e15c91e948c2a250b424
rojolocco <rojolocco@gmail.com>
2025-05-06 10:59:27 -0500

    First Commit

---
+++ src/store/useUserStore.ts
@@ -0,0 +1,20 @@
+import { create } from 'zustand'
+
+interface UserStoreState {
+    bears: number;
+}
+
+interface UserStoreActions {
+    increasePopulation: () => void;
+    removeAllBears: () => void;
+    updateBears: (newBears: number) => void;
+}
+
+const useStore = create<UserStoreState & UserStoreActions>((set) => ({
+    bears: 0,
+    increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
+    removeAllBears: () => set({ bears: 0 }),
+    updateBears: (newBears) => set({ bears: newBears }),
+}));
+
+export default useStore
